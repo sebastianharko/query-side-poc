0f69c7871d85979e1f6f2520cd3a84ee61439227
sebastian.harko <sebastian.harko@lightbend.com>
2017-03-15 12:05:31 +0100

    maybe working version

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,7 +7,7 @@ import akka.event.LoggingReceive
 import akka.pattern.ask
 import akka.persistence.PersistentActor
 import akka.stream._
-import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, RunnableGraph, Source}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Merge, RunnableGraph, Sink, Source, Zip}
 
 import scala.collection.mutable
 
@@ -17,6 +17,46 @@ case class AccountInformation(accountId: String,
 
 case class HolderAddedToAccount(accountId: String,
                                 newHolderId: String)
+
+
+object GlobalPositionActor {
+
+  def props = Props(new GlobalPositionActor)
+
+  val extractEntityId: ShardRegion.ExtractEntityId = {
+    case msg @ HolderAddedToAccount(_, newHolderId) ⇒ (newHolderId, msg)
+  }
+
+  val extractShardId: ShardRegion.ExtractShardId = {
+    case HolderAddedToAccount(accountId, newHolderId) => newHolderId.head.toString
+
+  }
+
+}
+
+
+class GlobalPositionActor extends PersistentActor with ActorLogging {
+
+  val myAccounts: mutable.Set[String] = mutable.Set()
+
+  override def persistenceId = self.path.name
+
+  override def receiveCommand = LoggingReceive {
+    case msg@HolderAddedToAccount(accountId, _) =>
+      persist(msg) {
+        event => {
+          myAccounts.add(accountId)
+          sender ! "Success"
+        }
+      }
+  }
+
+  override def receiveRecover = {
+    case HolderAddedToAccount(accountId, _) =>
+      myAccounts.add(accountId)
+  }
+
+}
 
 class AccountActor extends PersistentActor with ActorLogging {
 
@@ -41,6 +81,7 @@ class AccountActor extends PersistentActor with ActorLogging {
 
 object AccountActor {
 
+
   def props = Props(new AccountActor)
 
   val extractEntityId: ShardRegion.ExtractEntityId = {
@@ -52,6 +93,107 @@ object AccountActor {
 
   }
 }
+
+
+
+object Main {
+
+  implicit val system = ActorSystem("query-side-poc")
+
+  implicit val materializer = ActorMaterializer()
+
+  val shardingForAccounts = ClusterSharding(system).start(
+      typeName = "account",
+      entityProps = AccountActor.props,
+      settings = ClusterShardingSettings(system),
+      extractEntityId = AccountActor.extractEntityId,
+      extractShardId = AccountActor.extractShardId
+  )
+
+  val shardingForPosicionGlobal = ClusterSharding(system).start(
+    typeName = "posicion global",
+    entityProps = GlobalPositionActor.props,
+    settings = ClusterShardingSettings(system),
+    extractEntityId = GlobalPositionActor.extractEntityId,
+    extractShardId = GlobalPositionActor.extractShardId
+  )
+
+
+
+  val exampleEvent = HolderAddedToAccount(accountId = "12710", newHolderId = "74")
+
+
+  val source = Source.single(exampleEvent)
+
+  import scala.concurrent.duration._
+  implicit val timeout = akka.util.Timeout(10 seconds)
+
+  import akka.stream.scaladsl.GraphDSL.Implicits._
+
+  val graph =
+  RunnableGraph.fromGraph(GraphDSL.create() { implicit builder =>
+
+    val Src: Outlet[HolderAddedToAccount] = builder.add(source).out
+
+    val Emision =
+      builder.add(Broadcast[HolderAddedToAccount](2))
+
+    val Cambio
+      = builder.add(Flow[HolderAddedToAccount].map(item => item.copy(accountId = "ING-" + item.accountId)))
+
+    val PublicarCuenta
+      = builder.add(Flow[HolderAddedToAccount].mapAsync(10)(event =>
+          (shardingForAccounts ? event).mapTo[String]))
+
+    val PublicarPosicionGlobal
+      = builder.add(Flow[HolderAddedToAccount].mapAsync(10)(event =>
+         (shardingForPosicionGlobal ? event).mapTo[String]))
+
+    val Unir = builder.add(Zip[String, String])
+
+    val Final: FlowShape[(String, String), String]
+      = builder.add(Flow[(String, String)].map(tuple => {
+          val items = List(tuple._1, tuple._2)
+          if (items.distinct.size == 1 && items.head == "success")
+             "success"
+           else
+            "failure"
+         }
+    ))
+
+    val Sumidero = builder.add(Sink.foreach(println)).in
+
+    /*
+    val C: UniformFanInShape[Int, Int]    = builder.add(Merge[Int](2))
+    val D: FlowShape[Int, Int]            = builder.add(Flow[Int].map(_ + 1))
+    val E: UniformFanOutShape[Int, Int]   = builder.add(Balance[Int](2))
+    val F: UniformFanInShape[Int, Int]    = builder.add(Merge[Int](2))
+    val G: Inlet[Any]                     = builder.add(Sink.foreach(println)).in
+
+                     C     <~      F
+    A  ~>     B  ~>  C     ~>      F
+              B  ~>  D  ~>  E  ~>  F
+                            E  ~>  G
+
+    */
+
+    Src ~> Cambio ~> Emision ~> PublicarCuenta         ~> Unir.in0
+                     Emision ~> PublicarPosicionGlobal ~> Unir.in1
+                                                          Unir.out ~> Final ~> Sumidero
+
+    ClosedShape
+  })
+
+  graph.run()
+
+
+
+}
+
+
+
+
+
 
 /*
 case class TransactionEvent(transactionId: String,
@@ -70,68 +212,3 @@ class AccountService {
 */
 
 
-
-
-
-object Main {
-
-  implicit val system = ActorSystem("query-side-poc")
-
-  implicit val materializer = ActorMaterializer()
-
-  val shardingForAccounts = ClusterSharding(system).start(
-      typeName = "account",
-      entityProps = AccountActor.props,
-      settings = ClusterShardingSettings(system),
-      extractEntityId = AccountActor.extractEntityId,
-      extractShardId = AccountActor.extractShardId
-  )
-
-
-  val exampleEvent = HolderAddedToAccount(accountId = "12710", newHolderId = "74")
-
-
-  val source = Source.single(exampleEvent)
-
-  import akka.stream.scaladsl.GraphDSL.Implicits._
-  RunnableGraph.fromGraph(GraphDSL.create() { implicit builder =>
-
-    val A: Outlet[HolderAddedToAccount] = builder.add(source).out
-
-    val B: UniformFanOutShape[HolderAddedToAccount, HolderAddedToAccount] =
-      builder.add(Broadcast[HolderAddedToAccount](2))
-
-    val C: FlowShape[HolderAddedToAccount, HolderAddedToAccount]
-      = builder.add(Flow[HolderAddedToAccount].map(item => item.copy(accountId = "ING-" + item.accountId)))
-
-    val D: FlowShape[HolderAddedToAccount, String]
-      = builder.add(Flow[HolderAddedToAccount].mapAsync(10)(
-        event => (shardingForAccounts ? event).mapTo[String]
-    ))
-
-
-    /*
-    val C: UniformFanInShape[Int, Int]    = builder.add(Merge[Int](2))
-    val D: FlowShape[Int, Int]            = builder.add(Flow[Int].map(_ + 1))
-    val E: UniformFanOutShape[Int, Int]   = builder.add(Balance[Int](2))
-    val F: UniformFanInShape[Int, Int]    = builder.add(Merge[Int](2))
-    val G: Inlet[Any]                     = builder.add(Sink.foreach(println)).in
-
-                     C     <~      F
-    A  ~>     B  ~>  C     ~>      F
-              B  ~>  D  ~>  E  ~>  F
-                            E  ~>  G
-
-    */
-
-    A ~>  C ~> B ~> D
-               B
-
-    ClosedShape
-  })
-
-
-
-
-
-}
